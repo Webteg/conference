e2cec492b16bdddc1a4f999444dcfb760e2f840b
cibse <info.cibse@gmail.com>
2017-08-25 17:23:35 -0500

    Ajustes documentos descargables y fechas importantes

--- texts.js
+++ texts.js
@@ -49,7 +49,7 @@ var menu = {
 			"3": {"name":"ESELAW","longName":"Experimental Software Engineering", "url":"track3","leader":"NN","email":"","image":"3rc.jpg"},
 			"4": {"name":"Industry","longName":"Industry Presentations", "url":"track4","leader":"NN","email":"","image":"4rc.jpg"},
 			"5": {"name":"Tools","longName":"Tools and Demos Track", "url":"track5","leader":"NN","email":"","image":"5rc.jpg"},
-			"6": {"name":"Doc Symp","longName":"Doctoral Symposyum", "url":"track6","leader":"NN","email":"","image":"6rc.jpg"}
+			"6": {"name":"Doc Symp","longName":"Doctoral Symposium", "url":"track6","leader":"NN","email":"","image":"6rc.jpg"}
 		},
 		"trackst": {"name":"Tracks","url":"tracks"},
 		"tutorials":{"name":"Tutorials","url":"tutorials"},
