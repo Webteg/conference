937e62c79dcc11a313f30253924f472d8c5cc8a9
Nicolas Cardozo <n.cardozo@uniandes.edu.co>
2017-08-29 21:34:47 -0500

    Unifying chairs format

--- texts.js
+++ texts.js
@@ -49,14 +49,14 @@ var menu = {
 							"notification":"Notification:",
 							"camera":"Camera Ready:"},
 		"topics":{"name":"Topics"},
-		"pchairs":{"name":"Program chairs"},
-		"pchair":{"name":"Program chair"},
+		"pchairs":{"name":"Program Chairs"},
+		"pchair":{"name":"Program Chair"},
 		"program": {"name":"Program","url":"conference"},
 		"calendar": {"name":"Program outline","url":"outline"},
 		"programdl": {"name":"Complete Program","url":"outline"},
 		"accepted": {"name":"Accepted papers","url":"accepted"},
 		"timetable": {"name":"Timetable","url":"timeTable"},
-		"generalProgram": {"name":"General program","url":"timeTable"},
+		"generalProgram": {"name":"General Program","url":"timeTable"},
 		"sponsors": {"name":"Sponsors","url":"sponsors"},
 		"chair": "Chair",
 		"pc": {"name":"Program committee"},
@@ -83,12 +83,11 @@ var menu = {
 		"toolsPaperChair": {"name":"Tools Paper Chairs"},
 		"proStuVolunChair": {"name":"Proceedings and Student Volunteers Chair"},
 		"publicityCoChairs": {"name":"Publicity Chairs"},
-		"industryExhibition": {"name":"Industry Exhibition Chairs"},
 		"webChair": {"name":"Web Chair"},
 		"sponsors": {"name":"Sponsors", "url":"sponsors"},
 		"registration":{"name":"Registration", "url":"registration"},
 		"school":{"name":"School", "url":"school"},
-		"socialEvents":{"name":"Social events", "url":"socialEvents"},
+		"socialEvents":{"name":"Social Events", "url":"socialEvents"},
 		"venue":{"name":"Venue"},
 		"conferenceVenue":{"name":"Conference venue", "url":"conferenceVenue"},
 		"contact":{"name":"Contact", "url":"contact"},
@@ -215,62 +214,11 @@ var menu = {
 
 };
 
-
-	/*
-	$menu["sp":
-	$menu["sp"]["tracks": array("name":"1. Métodos formales, lógica computacional y teoría de la computación","url":"track1","leader":"Camilo Rocha","email":"camilo.rocha@escuelaing.edu.co","image":"1cr.jpg"),
-								array("name":"2. Ingenier&iacute;a de Software","url":"track2","leader":"Rubby Casallas","email":"rcasalla@uniandes.edu.co","image":"2rc.jpg"),
-								array("name":"8. Arquitectura y Sistemas de Información y Empresariales","url":"track8","leader":"Jorge Villalobos","email":"jvillalo@uniandes.edu.co","image":"8jv.jpg"),
-								array("name":"9. Computación de Alto Rendimiento y Computación en la Nube","url":"track9","leader":"Harold Castro","email":"hcastro@uniandes.edu.co","image":"9hc.jpg")
-	$menu["sp"]["trackst": "name":"L&iacute;neas","url":"tracks"
-	$menu["sp"]["home": "name":"Inicio","url":"home"
-	$menu["sp"]["committees": "name":"Comit&eacute;s","url":"committees"
-	$menu["sp"]["registration": "name":"Inscripci&oacute;n","url":"registration"
-	$menu["sp"]["tutorials": "name":"Tutoriales","url":"tutorials"
-	$menu["sp"]["keynotes": "name":"Keynotes","url":"keynotes"
-
-	$menu["sp"]["simposio": "name":"Simposio","url":"simposio"
-	$menu["sp"]["simposiodm": "name":"Simposio Doctoral y de Maestr&iacute;a","url":"simposio"
-
-	$menu["sp"]["conference": "name":"Conferencia","url":"conference"
-	$menu["sp"]["cfp": "name":"Llamado de trabajos","url":"cfp"
-	$menu["sp"]["dates": "name":"Fechas importantes","url":"dates",
-															"abstract":"Deadline abstract:",
-															"submission":"Deadline art&iacute;culo:",
-															"notification":"Notificaci&oacute;n:",
-															"camera":"Camera Ready:"
-
-	$menu["sp"]["program": "name":"Programa","url":"conference"
-	$menu["sp"]["calendar": "name":"Programa global","url":"outline"
-	$menu["sp"]["programdl": "name":"Programa detallado","url":"Folleto_PDF.pdf"
-	$menu["sp"]["accepted": "name":"Envíos aceptados","url":"accepted"
-
-	$menu["sp"]["accommodation": "name":"Hoteles","url":"accommodation"
-	$menu["sp"]["contact": "name":"Contacto","url":"contact"
-
-	$chaired["sp"]="L&iacute;der:";
-	$programCommittee["sp"]="Comité de programa";
-	$viewDetails["sp"]="Ver detalles";
-	$pageTitle["sp"] = "Conferencia Ibero-americana en Software Engineering";
-	$coChairs["sp"]="Co-directores";
-	$leaders["sp"]="Líderes de línea";
-	$organizers["sp"]="Organizadores";
-	$sponsors["sp"]="Patrocinadores";
-*/
-	//---- YEARLY CUSTOMIZATIONS ----
+//---- YEARLY CUSTOMIZATIONS ----
 var year = "2018";
 var country = "Colombia";
 
-/*
-	//SP
-	$menu["sp"]["university": "name":"Universidad de los Andes","url":"university"
-	$menu["sp"]["depto": "name":"Departamento de Ingeniería de Sistemas y Computación","url":"http://sistemas.uniandes.edu.co"
-	$menu["sp"]["edition"] = "XXI";
-	$menu["sp"]["venue": "name":"Sede","url":"venue"
-	$menu["sp"]["city": "name":"Bogot&aacute;","url":"bogota"
-	$menu["sp"]["date": "month":"Abril", "days":"26-30"
-*/
-	//General
+//General
 var universityURl = "http://uniandes.edu.co";
 var universityURl_en = "http://uniandes.edu.co/en";
 var easyChairURL = "http://easychair.com";
